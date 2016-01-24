6ee2a21aae7c11eaf63ccd259a65d5efb244a6e5
Alex Howington <alex.howington@gmail.com>
2016-01-23 18:47:51 -0600

    Add overlay on image hover on homepage

--- app/assets/javascripts/photos.js
+++ app/assets/javascripts/photos.js
@@ -15,3 +15,31 @@ $(document).ready(function() {
     }
   });
 });
+
+$(function() {
+  // Opacity of overlay set to 0%
+  $(".overlay").css("opacity","0");
+
+  // On image load, set width and height of overlay for each image
+  $('.home img').on('load',function(){
+    var w = $(this).css("width");
+    var h = $(this).css("height");
+    var r = $(this).siblings('.overlay');
+    r.css("height",h).css("width",w);
+  });
+   
+  // On mouse over
+  $(".overlay").hover(function () {
+      // Set width and height of overlay being hovered (in case screen size was changed since the first time dimensions were set)
+      var i = $(this).siblings('img');
+      var w = i.css("width");
+      var h = i.css("height");
+      $(this).css("height",h).css("width",w);
+      // Set opacity to 60%
+      $(this).stop().animate({opacity: 0.6}, "fast");
+    },
+    // On mouse out, set opacity back to 0%
+    function () {
+      $(this).stop().animate({opacity: 0}, "fast");
+  });
+});
