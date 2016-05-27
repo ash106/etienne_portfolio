2f4d9bfa6d803887cdec537284642f0444069f21
Alex Howington <alex.howington@gmail.com>
2016-05-27 01:20:29 -0500

    New paperclip styles to fix image layout issues

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -10,8 +10,9 @@ class Photo < ActiveRecord::Base
   ranks :skiing_order, scope: :skiing
 
   has_attached_file :image, styles: {
-    medium: '300x300>',
-    large: '800x800>'
+    small: '300>',
+    medium: '650>',
+    large: '1000>'
   }
 
   validates_attachment :image, presence: true, content_type: { content_type: /\Aimage\/.*\Z/ }
